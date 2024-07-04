9d4dd65a0a6966a8bfaeeeae40b0fb86f7433ae6
Qmm <1259598502@qq.com>
2024-07-05 00:27:00 +0800

    Updates

--- app/immutable/chunks/paths.VLhSmSXW.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1ofd5i0)==null?void 0:s.base)??"/Wordle";var e;const t=((e=globalThis.__sveltekit_1ofd5i0)==null?void 0:e.assets)??a;export{t as a,a as b};
--- app/immutable/entry/start.ChThzLTC.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.qA0A2LC9.js";export{t as start};
